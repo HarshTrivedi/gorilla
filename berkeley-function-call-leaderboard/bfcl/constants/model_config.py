5314110027b72d6435d09da40e826f0785fa9a8d
Shashank Gupta <shashankg@allenai.org>
2025-11-19 19:46:17 -0800

    changes to make some of the olmo3 baselines work

--- berkeley-function-call-leaderboard/bfcl/constants/model_config.py
+++ berkeley-function-call-leaderboard/bfcl/constants/model_config.py
@@ -30,6 +30,7 @@ from bfcl.model_handler.local_inference.gemma import GemmaHandler
 from bfcl.model_handler.local_inference.glaive import GlaiveHandler
 from bfcl.model_handler.local_inference.glm import GLMHandler
 from bfcl.model_handler.local_inference.granite import GraniteHandler
+from bfcl.model_handler.local_inference.granite_3 import Granite3FCHandler
 from bfcl.model_handler.local_inference.hammer import HammerHandler
 from bfcl.model_handler.local_inference.hermes import HermesHandler
 from bfcl.model_handler.local_inference.llama import LlamaHandler
@@ -1134,6 +1135,30 @@ local_inference_model_map = {
         is_fc_model=False,
         underscore_to_dot=True,
     ),
+    "ibm-granite/granite-3.2-8b-instruct": ModelConfig(
+        model_name="ibm-granite/granite-3.2-8b-instruct",
+        display_name="Granite-3.2-8B-Instruct (FC)",
+        url="https://huggingface.co/ibm-granite/granite-3.2-8b-instruct",
+        org="IBM",
+        license="Apache-2.0",
+        model_handler=Granite3FCHandler,
+        input_price=None,
+        output_price=None,
+        is_fc_model=True,
+        underscore_to_dot=False,
+    ),
+    "ibm-granite/granite-3.3-8b-instruct": ModelConfig(
+        model_name="ibm-granite/granite-3.3-8b-instruct",
+        display_name="Granite-3.3-8B-Instruct (FC)",
+        url="https://huggingface.co/ibm-granite/granite-3.3-8b-instruct",
+        org="IBM",
+        license="Apache-2.0",
+        model_handler=Granite3FCHandler,
+        input_price=None,
+        output_price=None,
+        is_fc_model=True,
+        underscore_to_dot=False,
+    ),
     "MadeAgents/Hammer2.1-7b": ModelConfig(
         model_name="MadeAgents/Hammer2.1-7b",
         display_name="Hammer2.1-7b (FC)",
@@ -1438,6 +1463,18 @@ local_inference_model_map = {
         model_name="Qwen/Qwen3-8B-FC",
         display_name="Qwen3-8B (FC)",
         url="https://huggingface.co/Qwen/Qwen3-8B",
+        org="Qwen",
+        license="apache-2.0",
+        model_handler=QwenFCHandler,
+        input_price=None,
+        output_price=None,
+        is_fc_model=True,
+        underscore_to_dot=False,
+    ),
+    "Qwen/Qwen3-VL-8B-Instruct": ModelConfig(
+        model_name="Qwen/Qwen3-VL-8B-Instruct",
+        display_name="Qwen3-VL-8B-Instruct (FC)",
+        url="https://huggingface.co/Qwen/Qwen3-VL-8B-Instruct",
         org="Qwen",
         license="apache-2.0",
         model_handler=QwenFCHandler,
